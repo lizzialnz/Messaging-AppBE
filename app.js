f6a65684cbbaed32ac8d9cd79dc7e87dd35960c2
Lizzi-Alonzo <lizzimalore@gmail.com>
2022-10-10 20:41:36 -0600

    final

--- app.js
+++ app.js
@@ -2,29 +2,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-// var cors = require('cors');
-
-// const corsWhiteList = (process.env.CORSLIST || '').split('|');
-// const corsOptions = {
-//   origin: (origin, next) => {
-//     console.log('CORS origin:', origin);
-//     if (corsWhiteList.includes(origin)) {
-//       next(null, true);
-//     } else {
-//       next(new Error('Not Allowed by Cors Policy'));
-//     }
-//   }
-// }
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var apiRouter = require('./routes/api');
 var app = express();
-app.set("port", process.env.port || 3000);
+// app.set("port", process.env.port || 3000);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 app.listen(app.get("port"), () => {
   console.log(`Server started on port: ${app.get("port")}`);
